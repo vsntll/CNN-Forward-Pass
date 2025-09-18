f2967880f9a4e6a14929d790d6eab59b4a0f57c7
Avie Vasantlal <avasantlal@gmail.com>
2025-09-17 22:47:04 -0400

    maxpool imp

--- applyfullconnect.m
+++ applyfullconnect.m
@@ -4,6 +4,7 @@ function outarray = applyfullconnect(inarray, filterbank, biasvals)
 % filterbank: NxMxD1xD2, biasvals: D2
 [N, M, D1] = size(inarray);
 [~, ~, ~, D2] = size(filterbank);
+
 outarray = zeros(1, 1, D2);
 for l = 1:D2
     filt = squeeze(filterbank(:, :, :, l));
---
+++ applymaxpool.m
@@ -0,0 +1,16 @@
+function outarray = applymaxpool(inarray)
+% Applies 2x2 max pooling
+% inarray: 2Nx2MxD, outarray: NxMxD
+[N2, M2, D] = size(inarray);
+N=N2 / 2;
+M=M2 / 2;
+outarray = zeroes(N,M,D);
+for k = 1:D
+    for i = 1:N
+        for j = 1:M
+            block = inarray(2*i-1:2*i, 2*j-1:2*j, k);
+            outarray(i, j, k) = max(block(:));
+        end
+    end
+end
+end
