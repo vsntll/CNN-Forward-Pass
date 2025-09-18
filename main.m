ac28b1ea21c047f4c568af00081924e0c268f16c
Michael-KS <mpkangspano@gmail.com>
2025-09-17 22:51:41 -0400

    fixed main layer order

--- main.m
+++ main.m
@@ -8,12 +8,11 @@ predicted_class = zeros(1, Nimages);
 
 for idx = 1:Nimages
     x = imageset(:, :, :, idx);                  % NxMx3 uint8 image
-    x = applyimnormalize(x);
+    x = applyimnormalize(x);                     % Layer 1
     
     % Example for two layers; expand sequence for all 18 layers
-    x = applyconvolve(x, conv1_filters, conv1_bias); % Layer 1
-    x = applyrelu(x);                                % Layer 2
-    x = applymaxpool(x);                             % Layer 3
+    x = applyconvolve(x, conv1_filters, conv1_bias); % Layer 2
+    x = applyrelu(x);                                % Layer 3
     x = applyconvolve(x, conv2_filters, conv2_bias); % Layer 4
     x = applyrelu(x);                                % Layer 5
     x = applymaxpool(x);                             % Layer 6
